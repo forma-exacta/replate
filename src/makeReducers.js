05de914c0c30768fd7e26677afebabd81b344f5e
Unknown <robert.mccracken@formaexacta.com>
2017-10-26 15:18:25 -0400

    basic collection reducers

--- src/makeReducers.js
+++ src/makeReducers.js
@@ -1,4 +1,4 @@
-import makeActionTypes from './makeActionTypes'
+import {makeCRUDActionTypes} from './makeActionTypes'
 
 export const makeReducer = (initialState, handlers) => {
   return function reducer(state = initialState, action) {
@@ -13,17 +13,32 @@ export const makeReducer = (initialState, handlers) => {
 
 export const makeCRUDReducers = (domainName) => {
 
-  const actionTypes = makeActionTypes(domainName)
+  const actionTypes = makeCRUDActionTypes(domainName)
 
-  const byId = makeReducer({byId: {}}, {
+  const byId = makeReducer({}, {
     [actionTypes.CREATE](state, action) {
-
-    }
+      return {...state, [action.payload._id]: action.payload}
+    },
+    [actionTypes.UPDATE](state, action) {
+      return {...state, [action.payload._id]: action.payload}
+    },
+    [actionTypes.DELETE](state, action) {
+      let newState = {...state}
+      delete newState[action.payload._id]
+      return newState
+    },
   })
 
-  const allIds = makeReducer({allIds: []}, {
+  const allIds = makeReducer([], {
     [actionTypes.CREATE](state, action) {
-
+      let newState = state.slice()
+      newState.push(action.payload._id)
+      return newState
+    },
+    [actionTypes.DELETE](state, action) {
+      let newState = state.slice();
+      newState.splice(newState.indexOf(action.payload._id), 1)
+      return newState
     }
   })
 
--- src/makeReducers.test.js
+++ src/makeReducers.test.js
@@ -1,5 +1,12 @@
 import {makeCRUDReducers} from './makeReducers'
 
+const initialState = {
+  byId: {
+    one: {_id: 'one'}
+  },
+  allIds: ['one']
+}
+
 describe('makeReducers', () => {
   it('has one test', () => {
     expect(true)
@@ -11,4 +18,78 @@ describe('makeReducers', () => {
       allIds: expect.any(Function)
     })
   })
+
+  it('CRUD handles create action', () => {
+    const reducers = makeCRUDReducers('test')
+    const createAction = {
+      type: 'TEST:CREATE',
+      payload: {
+        _id: 'two',
+      }
+    }
+
+
+    const newByIdState = reducers.byId(initialState.byId, createAction)
+    expect(newByIdState).toEqual({
+      ...initialState.byId,
+      two: {_id: 'two'}
+    })
+    // immutable
+    expect(initialState !== newByIdState)
+
+    const newAllIdsState = reducers.allIds(initialState.allIds, createAction)
+    expect(newAllIdsState).toEqual([
+      ...initialState.allIds,
+      'two'
+    ])
+    // immutable
+    expect(initialState !== newAllIdsState)
+  })
+
+  it('CRUD handles update action', () => {
+    const reducers = makeCRUDReducers('test')
+    const updateAction = {
+      type: 'TEST:UPDATE',
+      payload: {
+        _id: 'one',
+        newProp: 'newProp'
+      }
+    }
+
+
+    const newByIdState = reducers.byId(initialState.byId, updateAction)
+    expect(newByIdState).toEqual({
+      one: {
+        _id: 'one',
+        newProp: 'newProp'
+      }
+    })
+    // immutable
+    expect(initialState !== newByIdState)
+
+    const newAllIdsState = reducers.allIds(initialState.allIds, updateAction)
+    expect(initialState === newAllIdsState)
+  })
+
+  it('CRUD handles delete action', () => {
+    const reducers = makeCRUDReducers('test')
+    const deleteAction = {
+      type: 'TEST:DELETE',
+      payload: {
+        _id: 'one',
+      }
+    }
+
+
+    const newByIdState = reducers.byId(initialState.byId, deleteAction)
+    expect(newByIdState).toEqual({})
+    // immutable
+    expect(initialState !== newByIdState)
+
+    const newAllIdsState = reducers.allIds(initialState.allIds, deleteAction)
+    expect(newAllIdsState).toEqual([])
+    // immutable
+    expect(initialState !== newAllIdsState)
+  })
+
 })
